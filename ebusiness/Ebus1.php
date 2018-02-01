2dccc3f6ee7a0c2cf1db17e26eeacf83f27501db
Yi Xian Sun <117331683@umail.ucc.ie>
2018-02-01 09:45:38 +0000

    Adding ebusiness

---
+++ ebusiness/Ebus1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Select Product</title>
+        
+        <!---jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+    </head>
+    <body>
+        <h4>Select a Producet</h4>
+        
+        <br/>
+        
+        <form method="POST" action ="Ebus2.php">
+
+        <label for="saleforce">
+            <input type="radio" id="saleforce" name="product" checked onClick="disablebtnProceed()"/>
+            SaleForce @ $100
+        </label>
+        
+        <br/>
+        
+        <label for ="aws">
+            <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
+            AWS @ $300
+        </label>
+        
+        <br/>
+        <br/>
+        
+        <label for="subtotal">
+            Sub Total
+            <input type="text" id="subtotal" value="0.00" readonly/>
+        </label>
+        
+        <br/>
+        
+        <label for="total">
+            Total
+            <input type="text" id="total" value="0.00" readonly/>
+        </label>
+        
+        <br/>
+        
+        <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+        
+        </form>
+        
+        <br/>
+        <button onClick="calSub()">Calculate Cost</button>
+        <a role="button" href="Ebus1.php">Clear Choice</a>
+    </body>
+</html>>
