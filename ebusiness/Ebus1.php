36cf7ac7f591475733a5e41b8c0f7a15e4947adf
Yi Xian Sun <117331683@umail.ucc.ie>
2018-02-09 13:51:30 +0000

    Adding new styles and comments

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -1,84 +1,54 @@
 <!DOCTYPE html>
-
 <html>
     
     <head>
-        
-        <!-- Page title of the form -->
+        <!-- Title of the form -->
         <title>Select Product</title>
         
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="cost_calc.js"></script>
-        
     </head>
     
     <body>
-        
-        <!-- Heading on the form -->
+        <!-- Heading -->
         <h4><strong>Select a Product</strong></h4>
         
-        <!-- Layout of the Page -->
+        <!-- Layout of the Page and Proceed to Ebus2 when all actions completed -->
         <form method="POST" action="Ebus2.php">
 
-            <!--Display Radio Button -->
+            <!-- Saleforce Radio Button -->
                 <label for="salesforce">
                 <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
                 SaleForce @ $100
             </label>
-        
-            <!-- Blank lines -->
-            <br/>
-            <br/>
-            <br/>
-        
-            <!--Display Radio Button -->
+            
+            <!-- AWS Radio Button -->
             <label for ="aws">
                 <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
                 AWS @ $300
             </label>
-        
-            <!-- Blank lines -->
-            <br/>
-            <br/>
-            <br/>
-        
-            <!--Display Textbox -->
+           
+            <!-- Subtotal Textbox -->
             <label for="subtotal">
                 Sub Total
                 <input type="text" id="subtotal" value="0.00" readonly/>
             </label>
-        
-            <!-- Blank lines -->
-            <br/>
-            <br/>
-            <br/>
-        
-            <!--Display Textbox -->
+           
+            <!-- Total Textbox -->
             <label for="total">
                 Total
                 <input type="text" id="total" value="0.00" readonly/>
             </label>
         
-            <!-- Blank lines -->
-            <br/>
-            <br/>
-            <br/>
-        
-            <!--Display Button -->
+            <!-- Button to Submit -->
             <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
-        
+            
         </form>
         
-        <!-- Blank lines -->
-        <br/>
-        <br/>
-        <br/>
-        
-        <!--Display Button -->
+        <!-- Button to Calculate the total cost-->
         <button onClick="calcSub()">Calculate Cost</button>
         <a role="button" href="Ebus1.php">Clear Choice</a>
-        
     </body>
     
 </html>>
--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
-
 <html>
     
     <head>
-        
+        <!-- Title of the form -->
         <title>Enter Details</title>
         
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        
     </head>
     
     <body>
-        
+        <!-- Heading -->
         <h4>Please enter your payment details</h4>
         
+        <!-- Layout of the Page and Proceed to Ebus3 when all actions completed -->
         <form method="POST" action="Ebus3.php">
-
+            
+            <!-- PIN Textbox -->
             <label for="user_pin">PIN</label>
             <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4"/>
         
+            <!-- Button to Submit -->
             <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
-        
+            
         </form>
-        
         <br/>
         
+        <!-- Button to Validate -->
         <button onClick="validateDetails()">Validate</button>
         
        <script type="text/javascript" src="ebus2_validator.js"></script>
-       
     </body>
     
 </html>
